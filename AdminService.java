ecf5d9c56ee9c950239488c34da74b06c7b64022
david-89 <davkedj89@yahoo.com>
2018-04-30 20:37:38 +0200

    Delete AdminService.java

--- AdminService.java
+++
@@ -1,9 +0,0 @@
-
-package app.service;
-
-import app.model.AdminModel;
-
-public interface AdminService {
-    public void registerAdmin(AdminModel adminModel);
-    public boolean checkAdmin(AdminModel adminModel);
-}
